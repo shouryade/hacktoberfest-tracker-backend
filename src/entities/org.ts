72623c44da9fa84d28cc9bcf910070e85d9e4bbb
arshi45 <arshdeep0220@gmail.com>
2022-10-29 13:25:07 +0530

    Tables Update

--- src/entities/org.ts
+++ src/entities/org.ts
@@ -1,8 +1,12 @@
-import { Entity } from "typeorm";
+import { Entity,Column } from "typeorm";
 import { Base} from "./utils/base";
 
 @Entity('org') 
 export class Org extends Base{
 
-    
+    @Column()
+    totalRepos:number
+
+    @Column()
+    totalRepoList:object
 }
--- src/entities/utils/base.ts
+++ src/entities/utils/base.ts
@@ -3,8 +3,24 @@ import { BaseEntity,Column,Entity, PrimaryColumn } from "typeorm"
 @Entity()
 export class Base extends BaseEntity {
     
-    @PrimaryColumn()
+    @PrimaryColumn({})
     id:string
 
-    
+    @Column()
+    totalContributions:number
+
+    @Column()
+    totalCommits:number
+
+    @Column()
+    totalIssues:number
+
+    @Column()
+    contributorList:object
+
+    @Column()
+    issueList:object
+
+    @Column()
+    contributionList:object
 }
