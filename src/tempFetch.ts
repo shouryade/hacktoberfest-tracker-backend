45e1038c1c51a42f57d9b8f3e72713af33399ee3
arshi45 <arshdeep0220@gmail.com>
2022-11-16 15:42:59 +0530

    Added Routes

--- src/tempFetch.ts
+++ src/tempFetch.ts
@@ -1,5 +1,6 @@
 import { Octokit } from "octokit";
 import * as express from "express";
+import { count } from "console";
 
 const app = express();
 const octo = new Octokit();
@@ -12,20 +13,88 @@ type repo = {
     "issues":any
 }
 
+async function getCounts(org:string,repoName:string){
+
+    let members = await octo.request("GET /repos/{owner}/{repo}/contributors",{
+        owner:org,
+        repo:repoName
+    });
+
+    let issueList = await octo.request("GET /repos/{owner}/{repo}/issues",{
+        owner:org,
+        repo:repoName
+    });
+
+    let commits = await octo.request("GET /repos/{owner}/{repo}/commits",{
+        owner:org,
+        repo:repoName
+    });
+
+    return {
+        commits:commits.data.length,
+        members:members.data.length,
+        issues:issueList.data.length
+    }
+
+}
+
 async function getNames(username:string){
     let repos;
-    let actualRepo:string[] = [];
+    let actualRepo:{
+        commits:number,
+        issues:number,
+        contributors:number,
+        repoCount:number,
+        repos:{
+            name:string,
+            desc:string,
+            topics:string[],
+            link:string}[]
+    } = {
+        commits:0,
+        issues:0,
+        contributors:0,
+        repoCount:0,
+        repos:[]
+    };
 
     repos = await octo.request("GET /orgs/{owner}/repos",{
         owner:username
     });
 
+    let commits:number = 0;
+    let issues:number = 0;
+    let contributors:number = 0;
+    let repoCount:number = 0;
+
     for(var i=0;i<repos.data.length;i++){
 
         if(repos.data[i].topics.includes("hacktoberfest2022") || repos.data[i].topics.includes("hacktoberfest")){
-            actualRepo.push(repos.data[i].name);
+            
+            repoCount += 1;
+            let counts = await getCounts(username,repos.data[i].name);
+
+            issues += counts.issues;
+            contributors += counts.members;
+            commits += counts.commits;
+            
+            actualRepo.repos.push({
+                name:repos.data[i].name,
+                desc:repos.data[i].description,
+                topics:repos.data[i].topics,
+                link:repos.data[i].html_url
+            });
         }
     }
+
+    actualRepo = {
+        ...actualRepo,
+        commits:commits,
+        contributors:contributors,
+        repoCount:repoCount,
+        issues:issues
+    }
+
     console.log(actualRepo);
 
     return actualRepo;
@@ -37,7 +106,6 @@ app.get("/:org/:repo",async (req,res) => {
     const name = req.params.repo;
 
     let send:{
-        orgName:string,
         data:repo
     }
 
@@ -57,12 +125,11 @@ app.get("/:org/:repo",async (req,res) => {
     });
 
     send = {
-        orgName:org,
         data:{
             name:name,
             issues:issueList.data,
             members:members.data,
-            totalCommits:commits.data,
+            totalCommits:commits.data.length,
             totalIssues:issueList.data.length
         }  
     }
@@ -73,11 +140,21 @@ app.get("/:org/:repo",async (req,res) => {
 app.get("/:username",async (req,res) => {
    
     let username = req.params.username;
-    let names:string[] = await getNames(username);
+    let names:{
+        commits:number,
+        issues:number,
+        contributors:number,
+        repoCount:number,
+        repos:{
+            name:string,
+            desc:string,
+            topics:string[],
+            link:string}[]
+    } = await getNames(username);
 
     res.send({
         org:username,
-        name:names
+        orgData:names
     });
 });
 
