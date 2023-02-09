de0b02ecccf0c8450e0d4fdc48cf1547b4b61672
arshi45 <arshdeep0220@gmail.com>
2023-02-09 22:19:52 +0530

    ** Updates ** - Organized the updateRepo route

--- src/routes/updateOrg.ts
+++ src/routes/updateOrg.ts
@@ -8,13 +8,103 @@ import { contributors, issue, orgData } from './types';
 import { Issues } from '../entities/issues';
 
 const router = express.Router();
+const repoRepo = AppDataSource.getRepository(Repo);
+const contRepo = AppDataSource.getRepository(Contribution);
+const issueRepo = AppDataSource.getRepository(Issues);
+
+
+const udpateRepo = async (oldRepo,newRepo) => {
+
+    const repoData = await repoRepo.createQueryBuilder(
+        'repo'
+    )
+    .select('repo.id')
+    .where('repo.id = :id',{id:oldRepo.id})
+    .leftJoinAndSelect(
+        'repo.contributions',
+        'contributions'
+    )
+    .getOne()
+
+    const repoIssues = await repoRepo.createQueryBuilder(
+        'repo'
+    )
+    .select('repo.id')
+    .where('repo.id = :id',{id:oldRepo.id})
+    .leftJoinAndSelect(
+        'repo.issues',
+        'issues'
+    )
+    .getOne();
+
+    newRepo.issueList.forEach(async (issue:issue) => {
+        var result1 = repoIssues.issues.find((ele) => {
+            return ele.issuesNo = issue.number
+        });
+
+        if(result1){
+            result1.desc = issue.body;
+            result1.title = issue.title;
+            result1.user = issue.author.login;
+            result1.url = issue.url;
+
+            await issueRepo.save(result1);
+        }
+        else{
+            const newIssue = issueRepo.create({
+                issuesNo:issue.number,
+                title:issue.title,
+                user:issue.author.login,
+                desc:issue.body,
+                url:issue.url,
+                repo:oldRepo
+            });
+            await issueRepo.save(newIssue);
+        }
+    });
+
+    newRepo.contributors.forEach(async (cont:contributors) => {
+
+        var result1 = repoData.contributions.find((ele) => {
+            return ele.githubId = cont.login;
+        })
+
+        if(result1) {
+            //update old contribution
+            result1.contributions = cont.contributions;
+            result1.picLink = cont.avatar_url;
+            result1.profile_link = cont.html_url
+
+            await contRepo.save(result1);
+        }
+        else{
+            //create new contribution
+            const newContribution = contRepo.create({
+                contributions:cont.contributions,
+                picLink:cont.avatar_url,
+                githubId:cont.login,
+                profile_link:cont.html_url,
+                repo:oldRepo
+            });
+
+            await contRepo.save(newContribution);
+        }                    
+    });
+
+    //update it's values
+
+    oldRepo.totalCommits = newRepo.totalCommits,
+    oldRepo.desc = newRepo.description,
+    oldRepo.name = newRepo.name,
+    oldRepo.topics = newRepo.topics,
+    oldRepo.totalIssues = newRepo.openIssues,
+    oldRepo.totalPrOpen = newRepo.prOpen,
+    oldRepo.url = newRepo.url
+
+    await repoRepo.save(oldRepo);
+}
 
 const mainUpdate = async (data,orgR) => {
-
-    const orgRepo = AppDataSource.getRepository(Org);
-    const repoRepo = AppDataSource.getRepository(Repo);
-    const contRepo = AppDataSource.getRepository(Contribution);
-    const issueRepo = AppDataSource.getRepository(Issues);
 
     data.repos.forEach(async (repo) => {
             
@@ -26,95 +116,8 @@ const mainUpdate = async (data,orgR) => {
 
         if(result){
             
+            udpateRepo(result,repo);
             //first update it's contributions
-
-            const repoData = await repoRepo.createQueryBuilder(
-                'repo'
-            )
-            .select('repo.id')
-            .where('repo.id = :id',{id:result.id})
-            .leftJoinAndSelect(
-                'repo.contributions',
-                'contributions'
-            )
-            .getOne()
-
-            const repoIssues = await repoRepo.createQueryBuilder(
-                'repo'
-            )
-            .select('repo.id')
-            .where('repo.id = :id',{id:result.id})
-            .leftJoinAndSelect(
-                'repo.issues',
-                'issues'
-            )
-            .getOne();
-
-            repo.issueList.forEach(async (issue:issue) => {
-                var result1 = repoIssues.issues.find((ele) => {
-                    return ele.issuesNo = issue.number
-                });
-
-                if(result1){
-                    result1.desc = issue.body;
-                    result1.title = issue.title;
-                    result1.user = issue.author.login;
-                    result1.url = issue.url;
-
-                    await issueRepo.save(result1);
-                }
-                else{
-                    const newIssue = issueRepo.create({
-                        issuesNo:issue.number,
-                        title:issue.title,
-                        user:issue.author.login,
-                        desc:issue.body,
-                        url:issue.url,
-                        repo:result
-                    });
-                    await issueRepo.save(newIssue);
-                }
-            });
-
-            repo.contributors.forEach(async (cont:contributors) => {
-
-                var result1 = repoData.contributions.find((ele) => {
-                    return ele.githubId = cont.login;
-                })
-
-                if(result1) {
-                    //update old contribution
-                    result1.contributions = cont.contributions;
-                    result1.picLink = cont.avatar_url;
-                    result1.profile_link = cont.html_url
-
-                    await contRepo.save(result1);
-                }
-                else{
-                    //create new contribution
-                    const newContribution = contRepo.create({
-                        contributions:cont.contributions,
-                        picLink:cont.avatar_url,
-                        githubId:cont.login,
-                        profile_link:cont.html_url,
-                        repo:result
-                    });
-
-                    await contRepo.save(newContribution);
-                }                    
-            });
-
-            //update it's values
-
-            result.totalCommits = repo.totalCommits,
-            result.desc = repo.description,
-            result.name = repo.name,
-            result.topics = repo.topics,
-            result.totalIssues = repo.openIssues,
-            result.totalPrOpen = repo.prOpen,
-            result.url = repo.url
-
-            await repoRepo.save(result);
 
         }
         else{
@@ -163,7 +166,8 @@ const mainUpdate = async (data,orgR) => {
 }
 const updateOrg = async () => {
         const orgRepo = AppDataSource.getRepository(Org);
-    
+
+        //fetching data from DB to update
         const orgData = await orgRepo.createQueryBuilder(
             'org'
         )
@@ -174,7 +178,7 @@ const updateOrg = async () => {
         .take(10)
         .getMany()
         
-    
+        //for each org this will fetch data from github and update it
         orgData.forEach(orgR => {
             
             var data:orgData;
